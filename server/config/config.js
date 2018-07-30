f78653fc1302e4378c168f4cf6c91d19b99eddc4
Ingrid Salazar Echeverri <isalazare@pslcol.com.co>
2018-07-30 16:25:33 -0500

    REST Server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
  * Puerto
  */
 process.env.PORT = process.env.PORT || 3000;
+
+/**
+ * Entorno
+ */
+process.env.NODE_EV = process.env.NODE_EV || 'dev';
+
+/**
+ * Base de Datos
+ */
+let urlDB;
+if (process.env.NODE_EV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:Aleja1109@ds111059.mlab.com:11059/cafe';
+}
+
+process.env.URLDB = urlDB;
