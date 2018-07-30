9c8ff4242d1f8b3dc4f9126d935e79ad87f7b556
Ingrid Salazar Echeverri <isalazare@pslcol.com.co>
2018-07-30 17:00:42 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:Aleja1109@ds111059.mlab.com:11059/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
